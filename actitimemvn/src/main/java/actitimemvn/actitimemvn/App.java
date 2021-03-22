d8db1706c7ecb97c797c7568eb0401d65129239d
Abin <Abin@LAPTOP-T8PA4NDH>
2021-03-22 17:44:10 +0530

    first commit

---
+++ actitimemvn/src/main/java/actitimemvn/actitimemvn/App.java
@@ -0,0 +1,13 @@
+package actitimemvn.actitimemvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
